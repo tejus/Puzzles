d6c279722396fc43986dabc9291f23a4b2ca43ff
tejus <tejus.k.v@gmail.com>
2019-05-31 08:37:32 +0530

    Incremental Ranges 2 https://codegolf.stackexchange.com/questions/186233/incremental-ranges

--- src/puzzles/IncrementalRanges.kt
+++ src/puzzles/IncrementalRanges.kt
@@ -10,8 +10,19 @@ fun incrementalRanges(n: Int): Sequence<Int> {
     }
 }
 
+//https://codegolf.stackexchange.com/questions/186233/incremental-ranges
+fun incrementalRanges2(x: Int, n: Int): MutableList<Int> {
+    val list = mutableListOf<Int>()
+    for (v in 0 until x)
+        list.add(v / n + v % n + 1)
+    return list
+}
+
 fun main() {
     println(incrementalRanges(1).take(49).toList())
     println(incrementalRanges(2).take(100).toList())
     println(incrementalRanges(3).take(13).toList())
+    println(incrementalRanges2(49, 1))
+    println(incrementalRanges2(100, 2))
+    println(incrementalRanges2(13, 3))
 }
